805f1447d7b293d98135f381cd2cba4686854559
DTSDV <DavisTSDV@hotmail.com>
2017-07-06 21:50:53 -0400

    Add files via upload

---
+++ myflaskapp/data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'Article One',
+            'body':'This is a very weird test',
+            'author':'Brad Traversy',
+            'create_date':'04-25-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'This is a very weird test',
+            'author': 'Brad Pitt',
+            'create_date':'04-25-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'This is a very weird test',
+            'author': 'Mister Bean',
+            'create_date':'04-25-2017'
+        }
+    ]
+    return articles
